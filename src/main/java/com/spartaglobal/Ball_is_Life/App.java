962c29af078df0e410726e87cc17ff245b887a84
Bailee Beckles <bbeckles@spartaglobal.com>
2019-11-06 09:59:22 +0000

    first commit

---
+++ src/main/java/com/spartaglobal/Ball_is_Life/App.java
@@ -0,0 +1,13 @@
+package com.spartaglobal.Ball_is_Life;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
